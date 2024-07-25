f2ae1f84767da0f401e566cb73ef15d071c9c786
Noortheen Raja <jnoortheen@gmail.com>
2024-07-25 17:56:56 +0530

    feat: add parse_file

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
+use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
-use pyo3::exceptions::{PyValueError};
 
-use ruff_python_parser::{Parsed, ParseError};
-use ruff_python_ast::{Expr, Mod, ModExpression, ModModule, PySourceType, Suite};
+use ruff_python_ast::ModModule;
 use ruff_python_parser as parser;
-
-
 
 /// A Python module implemented in Rust.
 #[pymodule]
@@ -13,8 +10,20 @@ mod xonsh_rd_parser {
     use super::*;
 
     #[pyfunction] // This will be part of the module
-    fn parse_string<'py>(py: Python<'py>, src: &str) -> PyResult<ModModule> {
+    fn parse_string<'py>(src: &str) -> PyResult<ModModule> {
         let parsed = parser::parse_module(src);
+        if let Ok(parsed) = parsed {
+            return Ok(parsed.into_syntax());
+        } else {
+            let errors = parsed.unwrap_err();
+            return Err(PyErr::new::<PyValueError, _>(format!("{:?}", errors)));
+        }
+    }
+
+    #[pyfunction]
+    fn parse_file<'py>(path: &str) -> PyResult<ModModule> {
+        let src = std::fs::read_to_string(path).unwrap();
+        let parsed = parser::parse_module(&src);
         if let Ok(parsed) = parsed {
             return Ok(parsed.into_syntax());
         } else {
--- tests/test_bench.py
+++ tests/test_bench.py
@@ -1,3 +1,6 @@
+import pytest
+
+@pytest.mark.benchmark(group="parse_string")
 def test_parse_string(benchmark):
     @benchmark
     def main():
@@ -5,3 +8,14 @@ def test_parse_string(benchmark):
 
         src_txt = "print(1)"
         return parser.parse_string(src_txt)
+
+
+@pytest.mark.benchmark(group="parse_file")
+def test_parse_file(benchmark):
+    @benchmark
+    def main():
+        import xonsh_rd_parser as parser
+        from pathlib import Path
+
+        path = Path(__file__).parent / "test_bench.py"
+        return parser.parse_file(str(path))
