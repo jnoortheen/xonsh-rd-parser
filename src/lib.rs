8e548c575ee6494bfa5ab1e3feb2a7161df8200d
Noortheen Raja <jnoortheen@gmail.com>
2024-11-20 14:41:31 +0530

    feat: annotate error on source code using miette

--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,13 @@ mod xonsh_rd_parser {
 
     #[pyfunction] // This will be part of the module
     pub fn parse_string<'py>(py: Python<'py>, src: &str) -> PyResult<PyObject> {
-        return parse_str(py, src);
+        parse_str(py, src, None)
     }
 
     #[pyfunction]
     pub fn parse_file<'py>(py: Python<'py>, path: &str) -> PyResult<PyObject> {
         let src = std::fs::read_to_string(path).unwrap();
-        return parse_str(py, &src);
+        parse_str(py, &src, Some(path))
     }
 }
 
@@ -32,9 +32,7 @@ def foo():
 ";
         pyo3::prepare_freethreaded_python();
         Python::with_gil(|py| {
-            let parsed = xonsh_rd_parser::parse_string(
-                py,
-                src);
+            let parsed = xonsh_rd_parser::parse_string(py, src);
             assert!(parsed.is_ok());
         })
     }
--- src/parser.rs
+++ src/parser.rs
@@ -1,19 +1,58 @@
+use miette::{Diagnostic, NamedSource, Report, SourceSpan};
 use py_ast::ast_module::AstModule;
 use py_ast::to_ast::ToAst;
 use pyo3::exceptions::PySyntaxError;
 use pyo3::{PyObject, PyResult, Python};
+use ruff_text_size::{TextRange, TextSize};
+use thiserror::Error;
 
+#[derive(Debug, Error, Diagnostic)]
+#[error("{message}")]
+struct PyParserError {
+    message: String,
+    #[source_code]
+    src: NamedSource<String>,
+    #[label("{message}")]
+    span: SourceSpan,
+}
 
-pub fn parse_str(py: Python<'_>, src: &str) -> PyResult<PyObject> {
+fn annotate_err(msg: String, location: TextRange, src: &str, filename: &str) -> String {
+    let offset = location.start().to_usize();
+    let end_offset = location.end().to_usize();
+
+    let err = PyParserError {
+        message: msg,
+        src: NamedSource::new(filename.to_string(), src.to_string()),
+        span: (offset..end_offset).into(),
+    };
+
+    // Create a report and convert it to string
+    let report = Report::new(err);
+    format!("{:?}", report)
+}
+
+#[test]
+fn test_annotation() {
+    let text = r#"
+def foo(a: int b: str) -> int:
+    return a + b
+$
+"new line"
+    "#;
+    let result = annotate_err(
+        "exception and error".to_string(),
+        TextRange::new(TextSize::new(14), TextSize::new(20)),
+        text,
+        "test.py",
+    );
+    println!("{result}");
+}
+
+pub fn parse_str(py: Python<'_>, src: &str, filename: Option<&str>) -> PyResult<PyObject> {
     let parsed = ruff_python_parser::parse_module(src).map_err(|err| {
-        let filename = "<unknown>";
-        let lineno = err.location.start().to_u32();
-        let offset = err.location.end().to_u32();
-        // let end_lineno = err.location.end().to_u32();
-        // let end_offset = err.location.end().to_u32();
-        // let text = src.to_string();
         let msg = format!("{:?}", err.error);
-        PySyntaxError::new_err((msg, filename, lineno, offset))
+        let msg = annotate_err(msg, err.location, src, filename.unwrap_or("<string>"));
+        PySyntaxError::new_err(msg)
     })?;
     let tree = parsed.into_syntax();
     let module = AstModule::new(py)?;
