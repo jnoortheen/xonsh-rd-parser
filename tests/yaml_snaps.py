88ef8f9e013a4eb4ab4cfb21c85737d92d258d8a
Noortheen Raja <jnoortheen@gmail.com>
2024-11-26 21:12:30 +0530

    test: add xfail cases in yaml-snaps

--- tests/yaml_snaps.py
+++ tests/yaml_snaps.py
@@ -49,8 +49,11 @@ def yaml_line_items(*names: str):
             data = yaml.load(file)
         for case, lines in data.items():
             for idx, item in enumerate(lines):
+                kwargs = dict(id=f"{path.stem}-{case}-{idx}")
+                if case.startswith("_"):
+                    kwargs["marks"] = pytest.mark.xfail
                 exp = LineItem(path, item.get("exp", ""), case, idx)
-                yield pytest.param(item["inp"], exp, id=f"{path.stem}-{case}-{idx}")
+                yield pytest.param(item["inp"], exp, **kwargs)
 
 
 def pytest_addoption(parser):
