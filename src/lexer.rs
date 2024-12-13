b429e1bd9279e3529428ac0caa880d29adaf01cd
Noortheen Raja <jnoortheen@gmail.com>
2024-12-13 23:23:35 +0530

    feat: add lexing for subproc_tokens

--- src/lexer.rs
+++ src/lexer.rs
@@ -7,6 +7,7 @@ use ruff_python_parser::{lexer::Lexer, Mode};
 use ruff_text_size::TextRange;
 use ruff_text_size::TextSize;
 
+#[derive(Debug, Clone)]
 #[pyclass]
 pub(crate) struct Token {
     kind: TokenKind,
@@ -101,4 +102,181 @@ impl PyLexer {
             Ok(tokens)
         }
     }
+
+    #[pyo3(signature = (mincol = None, returnline = None, greedy = None, maxcol = None))]
+    fn subproc_toks(
+        &mut self,
+        py: Python<'_>,
+        mincol: Option<i64>,
+        returnline: Option<bool>,
+        greedy: Option<bool>,
+        maxcol: Option<usize>,
+    ) -> PyResult<Option<String>> {
+        let src = self.src.to_str(py)?;
+        let maxcol = maxcol.unwrap_or(src.len());
+        let mincol = mincol.unwrap_or(-1);
+        let returnline = returnline.unwrap_or(false);
+        let greedy = greedy.unwrap_or(false);
+        let mut tokens = self.tokens(py)?;
+        let result = if let Some(range) = tokens.find_subproc_line(mincol, maxcol, greedy) {
+            let line = format!("![{}]", &src[range]);
+            dbg!(range, &line);
+            if returnline {
+                let line = format!(
+                    "{}{}{}",
+                    &src[..range.start().to_usize()],
+                    line,
+                    &src[range.end().to_usize()..]
+                );
+                Some(line)
+            } else {
+                Some(line)
+            }
+        } else {
+            None
+        };
+        Ok(result)
+    }
+}
+
+trait LexerExt {
+    fn find_subproc_line(&mut self, mincol: i64, maxcol: usize, greedy: bool) -> Option<TextRange>;
+}
+
+impl LexerExt for Vec<Token> {
+    /// Encapsulates tokens in a source code line in a uncaptured
+    // subprocess ![] starting at a minimum column. If there are no tokens
+    // (ie in a comment line) this returns None. If greedy is True, it will encapsulate
+    // normal parentheses. Greedy is False by default.
+    fn find_subproc_line(&mut self, mincol: i64, maxcol: usize, greedy: bool) -> Option<TextRange> {
+        let mut toks: Vec<Token> = Vec::new();
+        let mut lparens = Vec::new();
+        let mut saw_macro = false;
+
+        let mut iterator = self.iter().peekable();
+        while let Some(token) = iterator.next() {
+            let tok = token.kind;
+            let pos = token.get_start();
+
+            if pos >= maxcol && !tok.is_proc_end() {
+                break;
+            }
+
+            if !saw_macro && tok.is_macro() {
+                saw_macro = true;
+            }
+
+            if saw_macro && !tok.is_macro_end() {
+                let start = token.range.start();
+                let mut end = token.range.end();
+                loop {
+                    match iterator.peek() {
+                        Some(t) if t.kind.is_macro_end() => {
+                            break;
+                        }
+                        Some(t) => {
+                            end = t.range.end();
+                            iterator.next(); // consume the token
+                        }
+                        None => break, // End of iterator
+                    }
+                }
+
+                let range = TextRange::new(start, end);
+                let new_token = Token {
+                    kind: TokenKind::String,
+                    range,
+                };
+                toks.push(new_token);
+                continue;
+            }
+
+            if tok.is_open_paren() {
+                lparens.push(tok);
+            }
+
+            if greedy && !lparens.is_empty() && lparens.contains(&TokenKind::Lpar) {
+                toks.push(token.clone());
+                if tok.is_rparen() {
+                    lparens.pop();
+                }
+                continue;
+            }
+
+            dbg!(&toks);
+            if let Some(last) = toks.last() {
+                if last.kind.is_proc_end() {
+                    if is_not_lparen_and_rparen(&lparens, &last.kind) {
+                        lparens.pop();
+                    } else {
+                        if pos < maxcol && !tok.is_macro_end() {
+                            if !greedy {
+                                toks.clear();
+                            }
+                            if tok.is_beg_skip() {
+                                continue;
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            } else if tok.is_beg_skip() {
+                continue;
+            }
+
+            if (pos as i64) < mincol {
+                continue;
+            }
+
+            toks.push(token.clone());
+
+            // if tok.type_ == "WS" && tok.value == "\\" {
+            //     continue;
+            // }
+            if matches!(tok, TokenKind::Newline | TokenKind::Dedent) {
+                break;
+            }
+            // if matches!(tok, TokenKind::Dedent) {
+            //     tok = handle_dedent_token(&mut toks, tok); //Needs Mutability fix
+            //     break;
+            // }
+            // if check_bad_str_token(&tok) {
+            //     return Ok(None);
+            // }
+        }
+
+        dbg!(&toks);
+        if let Some(last) = toks.last() {
+            if (last.kind.is_proc_end()
+                && !(is_not_lparen_and_rparen(&lparens, &last.kind)
+                    || (greedy && last.kind.is_rparen())))
+                || last.kind.is_any_newline()
+            {
+                toks.pop();
+            }
+        }
+
+        if let Some(start) = toks.first() {
+            let start = start.range.start();
+            if let Some(end) = toks.last() {
+                let end = end.range.end();
+                return Some(TextRange::new(start, end));
+            }
+        }
+
+        return None;
+    }
+}
+
+fn is_not_lparen_and_rparen(lparens: &[TokenKind], tok: &TokenKind) -> bool {
+    if tok != &TokenKind::Rpar {
+        return false;
+    }
+    for tok in lparens {
+        if tok != &TokenKind::Lpar {
+            return true;
+        }
+    }
+    return false;
 }
---
+++ tests/test_proc_toks.py
@@ -0,0 +1,362 @@
+import pytest
+from xonsh_rd_parser import Lexer
+
+INDENT = "    "
+
+
+def subproc_toks(inp, returnline=False, greedy=False, mincol=0, maxcol=None):
+    lexer = Lexer(inp)
+    return lexer.subproc_toks(
+        returnline=returnline, greedy=greedy, mincol=mincol, maxcol=maxcol
+    )
+
+
+@pytest.mark.parametrize(
+    "inp",
+    [
+        "x",
+        "ls -l",
+        "git commit -am 'hello doc'",
+    ],
+)
+@pytest.mark.parametrize("end", ["\n", ";", ""])
+def test_subproc_toks(inp, end):
+    exp = f"![{inp}]{end}"
+    assert subproc_toks(inp + end, returnline=True) == exp
+
+
+def test_bash_macro():
+    s = "bash -c ! export var=42; echo $var"
+    exp = f"![{s}]\n"
+    obs = subproc_toks(s + "\n", returnline=True)
+    assert exp == obs
+
+
+def test_python_macro():
+    s = 'python -c ! import os; print(os.path.abspath("/"))'
+    exp = f"![{s}]\n"
+    obs = subproc_toks(s + "\n", returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_indent_ls():
+    s = "ls -l"
+    exp = INDENT + f"![{s}]"
+    obs = subproc_toks(INDENT + s, mincol=len(INDENT), returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_indent_ls_nl():
+    s = "ls -l"
+    exp = INDENT + f"![{s}]\n"
+    obs = subproc_toks(INDENT + s + "\n", mincol=len(INDENT), returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_indent_ls_no_min():
+    s = "ls -l"
+    exp = INDENT + f"![{s}]"
+    obs = subproc_toks(INDENT + s, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_indent_ls_no_min_nl():
+    s = "ls -l"
+    exp = INDENT + f"![{s}]\n"
+    obs = subproc_toks(INDENT + s + "\n", returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_indent_ls_no_min_semi():
+    s = "ls"
+    exp = INDENT + f"![{s}];"
+    obs = subproc_toks(INDENT + s + ";", returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_indent_ls_no_min_semi_nl():
+    s = "ls"
+    exp = INDENT + f"![{s}];\n"
+    obs = subproc_toks(INDENT + s + ";\n", returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_ls_comment():
+    s = "ls -l"
+    com = "  # lets list"
+    exp = f"![{s}]{com}"
+    obs = subproc_toks(s + com, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_ls_42_comment():
+    s = "ls 42"
+    com = "  # lets list"
+    exp = f"![{s}]{com}"
+    obs = subproc_toks(s + com, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_ls_str_comment():
+    s = 'ls "wakka"'
+    com = "  # lets list"
+    exp = f"![{s}]{com}"
+    obs = subproc_toks(s + com, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_indent_ls_comment():
+    ind = "    "
+    s = "ls -l"
+    com = "  # lets list"
+    exp = f"{ind}![{s}]{com}"
+    obs = subproc_toks(ind + s + com, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_indent_ls_str():
+    ind = "    "
+    s = 'ls "wakka"'
+    com = "  # lets list"
+    exp = f"{ind}![{s}]{com}"
+    obs = subproc_toks(ind + s + com, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_ls_l_semi_ls_first():
+    lsdl = "ls -l"
+    ls = "ls"
+    s = f"{lsdl}; {ls}"
+    exp = f"![{lsdl}]; {ls}"
+    obs = subproc_toks(s, maxcol=6, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_ls_l_semi_ls_second():
+    lsdl = "ls -l"
+    ls = "ls"
+    s = f"{lsdl}; {ls}"
+    exp = f"{lsdl}; ![{ls}]"
+    obs = subproc_toks(s, mincol=7, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_hello_mom_first():
+    fst = "echo 'hello'"
+    sec = "echo 'mom'"
+    s = f"{fst}; {sec}"
+    exp = f"![{fst}]; {sec}"
+    obs = subproc_toks(s, maxcol=len(fst) + 1, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_hello_mom_second():
+    fst = "echo 'hello'"
+    sec = "echo 'mom'"
+    s = f"{fst}; {sec}"
+    exp = f"{fst}; ![{sec}]"
+    obs = subproc_toks(s, mincol=len(fst), returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_hello_bad_leading_single_quotes():
+    obs = subproc_toks('echo "hello', returnline=True)
+    assert obs is None
+
+
+def test_subproc_toks_hello_bad_trailing_single_quotes():
+    obs = subproc_toks('echo hello"', returnline=True)
+    assert obs is None
+
+
+def test_subproc_toks_hello_bad_leading_triple_quotes():
+    obs = subproc_toks('echo """hello', returnline=True)
+    assert obs is None
+
+
+def test_subproc_toks_hello_bad_trailing_triple_quotes():
+    obs = subproc_toks('echo hello"""', returnline=True)
+    assert obs is None
+
+
+def test_subproc_toks_hello_mom_triple_quotes_nl():
+    s = 'echo """hello\nmom"""'
+    exp = f"![{s}]"
+    obs = subproc_toks(s, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_comment():
+    exp = None
+    obs = subproc_toks("# I am a comment", returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_not():
+    exp = "not ![echo mom]"
+    obs = subproc_toks("not echo mom", returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_paren():
+    exp = "(![echo mom])"
+    obs = subproc_toks("(echo mom)", returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_paren_ws():
+    exp = "(![echo mom])  "
+    obs = subproc_toks("(echo mom)  ", returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_not_paren():
+    exp = "not (![echo mom])"
+    obs = subproc_toks("not (echo mom)", returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_and_paren():
+    exp = "True and (![echo mom])"
+    obs = subproc_toks("True and (echo mom)", returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_paren_and_paren():
+    exp = "(![echo a]) and (echo b)"
+    obs = subproc_toks("(echo a) and (echo b)", maxcol=9, returnline=True)
+    assert obs == exp
+
+
+def test_subproc_toks_semicolon_only():
+    exp = None
+    obs = subproc_toks(";", returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_pyeval():
+    s = "echo @(1+1)"
+    exp = f"![{s}]"
+    obs = subproc_toks(s, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_pyeval_multiline_string():
+    s = 'echo @("""hello\nmom""")'
+    exp = f"![{s}]"
+    obs = subproc_toks(s, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_twopyeval():
+    s = "echo @(1+1) @(40 + 2)"
+    exp = f"![{s}]"
+    obs = subproc_toks(s, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_pyeval_parens():
+    s = "echo @(1+1)"
+    inp = f"({s})"
+    exp = f"(![{s}])"
+    obs = subproc_toks(inp, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_twopyeval_parens():
+    s = "echo @(1+1) @(40+2)"
+    inp = f"({s})"
+    exp = f"(![{s}])"
+    obs = subproc_toks(inp, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_pyeval_nested():
+    s = "echo @(min(1, 42))"
+    exp = f"![{s}]"
+    obs = subproc_toks(s, returnline=True)
+    assert exp == obs
+
+
+@pytest.mark.parametrize(
+    "phrase",
+    [
+        "xandy",
+        "xory",
+        "xand",
+        "andy",
+        "xor",
+        "ory",
+        "x-and",
+        "x-or",
+        "and-y",
+        "or-y",
+        "x-and-y",
+        "x-or-y",
+        "in/and/path",
+        "in/or/path",
+    ],
+)
+def test_subproc_toks_and_or(phrase):
+    s = "echo " + phrase
+    exp = f"![{s}]"
+    obs = subproc_toks(s, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_pyeval_nested_parens():
+    s = "echo @(min(1, 42))"
+    inp = f"({s})"
+    exp = f"(![{s}])"
+    obs = subproc_toks(inp, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_capstdout():
+    s = "echo $(echo bat)"
+    exp = f"![{s}]"
+    obs = subproc_toks(s, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_capproc():
+    s = "echo !(echo bat)"
+    exp = f"![{s}]"
+    obs = subproc_toks(s, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_pyeval_redirect():
+    s = 'echo @("foo") > bar'
+    inp = f"{s}"
+    exp = f"![{s}]"
+    obs = subproc_toks(inp, returnline=True)
+    assert exp == obs
+
+
+def test_subproc_toks_greedy_parens():
+    s = "(sort)"
+    exp = f"![{s}]"
+    obs = subproc_toks(s, returnline=True, greedy=True)
+    assert exp == obs
+
+
+def test_subproc_toks_greedy_parens_inp():
+    s = "(sort) < input.txt"
+    exp = f"![{s}]"
+    obs = subproc_toks(s, returnline=True, greedy=True)
+    assert exp == obs
+
+
+def test_subproc_toks_greedy_parens_statements():
+    s = '(echo "abc"; sleep 1; echo "def")'
+    exp = f"![{s}]"
+    obs = subproc_toks(s, returnline=True, greedy=True)
+    assert exp == obs
+
+
+def test_subproc_toks_greedy_parens_statements_with_grep():
+    s = '(echo "abc"; sleep 1; echo "def") | grep'
+    exp = f"![{s}]"
+    obs = subproc_toks(s, returnline=True, greedy=True)
+    assert obs == exp
