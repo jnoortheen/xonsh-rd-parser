e3b324221339d0e5c22895f8271453ed5bbabd62
Alex Waygood <Alex.Waygood@Gmail.com>
2026-05-28 11:51:20 +0100

    Improve diagnostics for syntax errors in forward annotations (#25158)
    ## Summary
    
    Fixes https://github.com/astral-sh/ty/issues/1627.
    
    Here's an example diagnostic with the current release of ty:
    
    <img width="2286" height="286" alt="image"
    src="https://github.com/user-attachments/assets/46ab9154-aaf0-4451-b301-5ff12fcd8507"
    />
    
    On this branch, this diagnostic becomes:
    
    <img width="1464" height="408" alt="image"
    src="https://github.com/user-attachments/assets/449e0b6c-58d2-4501-91df-c267db6f48ca"
    />
    
    The exact span of the node that creates the syntax error is now retained
    and highlighted in the diagnostic.
    
    ## Implementation
    
    Propagating the range of the node inside the string annotation into the
    diagnostic is trivial. However, naively implementing that quickly
    revealed that this would make diagnostics like this unsuppressable:
    
    ```py
    x: """list[
        yield from range(42)
    ]"""
    ```
    
    The primary range of the diagnostic is now specifically the `yield from
    range(42)` part of the string rather than the string node as a whole.
    But I cannot add a `ty: ignore` comment that is either on or above the
    `yield from range(42)` part of the string -- the "comment" there would
    just become part of the string.
    
    This PR also improves the consistency of our parser error messages in
    general when it comes to capitalization.
    
    ## Test Plan
    
    Mdtests extended and updated
    
    ---------
    
    Co-authored-by: Micha Reiser <micha@reiser.io>

--- crates/ruff_python_parser/src/error.rs
+++ crates/ruff_python_parser/src/error.rs
@@ -106,6 +106,9 @@ pub enum ParseErrorType {
     /// An unexpected error occurred.
     OtherError(String),
 
+    /// An error specific to stringified annotations occurred.
+    StringAnnotationError(&'static str),
+
     /// An empty slice was found during parsing, e.g `data[]`.
     EmptySlice,
     /// An empty global names list was found during parsing.
@@ -221,7 +224,8 @@ impl std::error::Error for ParseErrorType {}
 impl std::fmt::Display for ParseErrorType {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            ParseErrorType::OtherError(msg) => write!(f, "{msg}"),
+            ParseErrorType::OtherError(msg) => f.write_str(msg),
+            ParseErrorType::StringAnnotationError(msg) => f.write_str(msg),
             ParseErrorType::ExpectedToken { found, expected } => {
                 write!(f, "Expected {expected}, found {found}")
             }
